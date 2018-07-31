91b627c59a02f30ae04ef59abf22a06662d3a4ad
Wigginns <3180645+Wigginns@users.noreply.github.com>
2018-07-31 15:29:53 -0600

    Undo threshing test changes.

--- ocr.py
+++ ocr.py
@@ -21,9 +21,7 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # check to see if we should apply thresholding to preprocess the
 # image
 if args["preprocess"] == "thresh":
-	# gray = cv2.threshold(gray, 0, 255,
-	# 	cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-    gray = cv2.threshold(gray, 0, 125,
+	gray = cv2.threshold(gray, 0, 255,
 		cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
 # make a check to see if median blurring should be done to remove

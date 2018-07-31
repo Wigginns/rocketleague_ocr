9f26f7ccea5edf7935508b329236040cbd200663
Wigginns <3180645+Wigginns@users.noreply.github.com>
2018-07-31 16:39:13 -0600

    different threshing tests

--- ocr.py
+++ ocr.py
@@ -16,13 +16,17 @@ args = vars(ap.parse_args())
 
 # load the example image and convert it to grayscale
 image = cv2.imread(args["image"])
+# gray = cv2.medianBlur(image,5)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # check to see if we should apply thresholding to preprocess the
 # image
 if args["preprocess"] == "thresh":
-	gray = cv2.threshold(gray, 0, 255,
-		cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+	th1 = cv2.threshold(gray, 0, 255,
+		cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    ####adaptiveThreshold(	src, maxValue, adaptiveMethod, thresholdType, blockSize, C)
+    # th1 = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+    #         cv2.THRESH_BINARY_INV,31,1)
 
 # make a check to see if median blurring should be done to remove
 # noise
@@ -32,7 +36,7 @@ elif args["preprocess"] == "blur":
 # write the grayscale image to disk as a temporary file so we can
 # apply OCR to it
 filename = "{}.png".format(os.getpid())
-cv2.imwrite(filename, gray)
+cv2.imwrite(filename, th1)
 
 # load the image as a PIL/Pillow image, apply OCR, and then delete
 # the temporary file
@@ -44,5 +48,6 @@ os.remove(filename)
 
 # show the output images
 cv2.imshow("Image", image)
-cv2.imshow("Output", gray)
+# cv2.imshow("Gray", gray)
+cv2.imshow("Output", th1)
 cv2.waitKey(0)
